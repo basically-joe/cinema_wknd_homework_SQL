d5180bf729dfcf2a5b26a68414df09bee6da55eb
Joe McColl <joe4kool@gmail.com>
2018-12-08 17:52:08 +0000

    added films method to customer.rb and able to see which which film customer has booked to see in pry

--- models/customer.rb
+++ models/customer.rb
@@ -44,4 +44,14 @@ class Customer
     SqlRunner.run(sql)
   end
 
+  def films()
+    sql = "SELECT films.* FROM
+    films INNER JOIN tickets
+    ON films.id = tickets.film_id
+    WHERE customer_id = $1"
+    values = [@id]
+    film_data = SqlRunner.run(sql, values)
+    return film_data.map{ |film| Film.new(film) }
+  end
+
 end
