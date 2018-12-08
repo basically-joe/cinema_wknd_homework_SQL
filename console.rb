1e6fecb4199d9ce7f7b765b955ecadbf68a9ee33
Joe McColl <joe4kool@gmail.com>
2018-12-08 17:32:07 +0000

    tickets has save method and working in console, pry and postico

--- console.rb
+++ console.rb
@@ -1,6 +1,6 @@
 require_relative("models/customer")
 require_relative("models/film")
-require_relative("models/tickets")
+require_relative("models/ticket")
 
 require('pry')
 
@@ -71,6 +71,18 @@ film3.save
 # film3.update
 #
 # film3.delete()
+
+ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket2 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film1.id})
+ticket3 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film2.id})
+ticket4 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film2.id})
+ticket5 = Ticket.new({'customer_id' => customer4.id, 'film_id' => film3.id})
+
+ticket1.save
+ticket2.save
+ticket3.save
+ticket4.save
+ticket5.save
 
 binding.pry
 nil
