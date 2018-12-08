3aabe912d72eaccc74d7f58d1790a9683a16f16f
Joe McColl <joe4kool@gmail.com>
2018-12-08 17:33:01 +0000

    ticket has select all method and tests pass in console and pry

--- models/ticket.rb
+++ models/ticket.rb
@@ -20,12 +20,12 @@ class Ticket
     @id = ticket['id'].to_i
   end
 
-  # def self.all()
-  #   sql = "SELECT * from tickets"
-  #   ticket_data = SqlRunner.run(sql)
-  #   return ticket_data.map{ |ticket| Ticket.new(ticket)}
-  # end
-  #
+  def self.all()
+    sql = "SELECT * from tickets"
+    ticket_data = SqlRunner.run(sql)
+    return ticket_data.map{ |ticket| Ticket.new(ticket)}
+  end
+
   # def update()
   #   sql = "UPDATE tickets SET (customer_id, film_id) = ($1, $2)
   #   WHERE id = $3"
